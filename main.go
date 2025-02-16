693c60938b460ecbf35e14e65bfcf46bcd1ec91d
Dat Doan <dat.doantrongk57@gmail.com>
2025-02-16 23:39:57 +0700

    Add code get all story

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello")
+	// fmt.Println("Hello")
 	// Request the html page
 	res, err := http.Get("https://truyenfull.vision")
 	if err != nil {
@@ -25,9 +25,45 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	// Get 'the loai' links
+	var links []string
 	doc.Find("li.dropdown").Each(func(i int, s *goquery.Selection) {
 		if strings.Contains(s.Text(), "Thể loại") {
-
+			// sHtml, _ := s.Html()
+			// fmt.Println(sHtml)
+			s.Find("a").Each(func(i int, s *goquery.Selection) {
+				link, exists := s.Attr("href")
+				if exists && !strings.HasPrefix(link, "javascript") {
+					links = append(links, link)
+				}
+			})
 		}
 	})
+	// fmt.Print(links)
+	// Loop get list story in each 'the loai'
+	for _, link := range links {
+		resLink, err := http.Get(link)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resLink.Body.Close()
+		if resLink.StatusCode != 200 {
+			log.Fatalf("Status code error: %d %s %s", res.StatusCode, res.Status, link)
+		}
+		doc, err := goquery.NewDocumentFromReader(resLink.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		//fmt.Println(doc.Html())
+		var storyLinks []string
+		doc.Find("h3.truyen-title a").Each(func(i int, s *goquery.Selection) {
+			// sHtml, _ := s.Html()
+			// fmt.Println(sHtml)
+			link, exists := s.Attr("href")
+			if exists && !strings.HasPrefix(link, "javascript") {
+				links = append(storyLinks, link)
+				fmt.Println(link)
+			}
+		})
+	}
 }
